9ee01890ddfc9c1584909f6b401fef64adfc7797
Alex Garel <alex@garel.org>
2018-06-20 16:15:56 +0100

    added doc strings

--- dask_igzip/text.py
+++ dask_igzip/text.py
@@ -17,21 +17,28 @@ delayed = delayed(pure=True)
 
 
 class InfosManager:
-    # making it a separate class for clarity
+    """Manage infos about lines
+    """
 
     @property
     def lines_index_path(self):
+        """path of the lines index
+        """
         return "%s.lines-index-%d" % (self.urlpath, self.chunk_size)
 
     _lines_infos = None
 
     def set_lines_info(self, count, line_index):
+        """register lines infos
+        """
         with open(self.lines_index_path, "w") as f:
             # put data in a coherent order to avoid loading too much data if not needed
             f.write(json.dumps({"count": count, "chunks": len(line_index)}) + "\n")
             f.write(json.dumps({'index': line_index}) + "\n")
 
     def get_lines_info(self, name):
+        """loads lines info, trying to avoid loading full index if not necessary
+        """
         # load index only if needed
         if self._lines_infos is not None:
             try:
@@ -49,18 +56,36 @@ class InfosManager:
 
     @property
     def lines_index(self):
+        """the lines index giving start of chunks
+        """
         return self.get_lines_info("index")
 
     @property
     def lines_count(self):
+        """number of lines in file
+        """
         return self.get_lines_info("count")
 
     @property
     def chunks_count(self):
+        """number of chunks file will be split into
+        """
         return self.get_lines_info("chunks")
 
 
 class IGzipReader(InfosManager):
+    """Indexed Gzip Reader.
+
+    It handles informations on the file,
+    as well as indexing,
+    and reading a chunk.
+
+    :param str urlpath: path of file
+    :param int chunk_size: number of line per chunk
+    :param spacing: optional param for indexed_gzip,
+      specifying the density of index
+      (a low number means a bigger index with more speed potential)
+    """
 
     def __init__(self, urlpath, chunk_size, spacing=1048576):
         self.urlpath = urlpath
@@ -69,9 +94,15 @@ class IGzipReader(InfosManager):
 
     @property
     def igzip_index_path(self):
+        """path of the gzip index
+        """
         return "%s.gzidx" % (os.path.splitext(self.urlpath)[0],)
 
     def ensure_indexes(self):
+        """build indexes if they don't exists yet, and save them.
+
+        Indexes depends on spacing, and chunk_size.
+        """
         if not os.path.exists(self.igzip_index_path):
             log.warning("Generating gzip index for %s" % self.urlpath)
             with igzip.IndexedGzipFile(self.urlpath, spacing=self.spacing) as fobj:
@@ -89,6 +120,12 @@ class IGzipReader(InfosManager):
             self.set_lines_info(count, line_index)
 
     def __call__(self, chunk, limit=None):
+        """read chunk in file
+
+        :param int chunk: chunk number
+        :param int limit: maximum number of lines to read. May be None.
+        :return list: read lines
+        """
         # read chunk
         line_index = self.lines_index
         start = line_index[chunk]
@@ -102,10 +139,25 @@ class IGzipReader(InfosManager):
 
 
 def _read_chunk(urlpath, chunk_size, chunk, limit=None):
+    """read a chunk in file.
+
+    :param str urlpath: file path
+    :param int chunk_size: number of lines per chunk
+    :param int chunk: chunk number to read
+    :prama int limit: if not None, limit the number of lines read
+
+    :return list: read lines
+    """
     return IGzipReader(urlpath=urlpath, chunk_size=chunk_size)(chunk, limit)
 
 
 def read_lines(urlpath, chunk_size=None, storage_options=None, limit=None):
+    """build read lines delayed for a set in a set of file
+
+    For parameters, see :py:func:`read_text`
+
+    :return list: delayed reads
+    """
     spacing = storage_options.get("index_spacing", 1048576) if storage_options else 1048576
     fs, fs_token, paths = dask.bytes.core.get_fs_token_paths(
         urlpath, mode='rb', storage_options=storage_options)
@@ -145,6 +197,24 @@ def read_lines(urlpath, chunk_size=None, storage_options=None, limit=None):
 
 def read_text(urlpath, collection=True, chunk_size=None, storage_options=None,
               encoding=None, errors='strict', limit=None):
+    """Given a collection of urls corresponding to gzip files,
+    read their lines, and build a bag.
+
+    :param urlpath: file(s) path
+    :type urlpath: str or list of str
+    :param bool collection: if False return a list of delayed
+    :param chunk_size: number of lines per chunk
+    :param storage_options: you may set the "spacing" for indexed gzip here.
+    :param encoding: characters encoding, if None returns bytes
+    :param errors: how to treat encoding errors
+    :param limit: limit the global number of lines read
+
+    :return: a bag or list of delayed
+
+    .. note::
+
+        `chunk_size` is an indication, on files boundary chunks are smaller.
+    """
     _, blocks = read_lines(
         urlpath, chunk_size=chunk_size, storage_options=storage_options, limit=limit)
 
@@ -160,4 +230,5 @@ def read_text(urlpath, collection=True, chunk_size=None, storage_options=None,
 
 
 def decode(lines, encoding, errors):
+    # small decoder
     return [line.decode(encoding, errors) for line in lines]
